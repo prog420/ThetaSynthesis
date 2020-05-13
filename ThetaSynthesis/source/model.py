5d7812615b0542c67330b890cc60425ce3f8585e
dcloudf <murkyrussian@gmail.com>
2020-05-13 21:35:30 +0300

    intermediate step for the implementation of synthon

---
+++ ThetaSynthesis/source/model.py
@@ -0,0 +1,28 @@
+from torch.nn import Sequential, Linear, Sigmoid, ReLU, Module
+
+
+class Chem(Module):
+    def __init__(self, in_f, num_rules):
+        super().__init__()
+        self.in_f = in_f
+        self.num_rules = num_rules
+        self.model_body = Sequential(
+            Linear(in_f, 4000),
+            ReLU(inplace=True),
+            Linear(4000, num_rules - 1)
+        )
+        self.rules_head = Sigmoid()
+
+        self.value_head = Sequential(
+            ReLU(),
+            Linear(num_rules - 1, 1000),
+            ReLU(inplace=True),
+            Linear(1000, 1),
+            Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.model_body(x)
+        rules = self.rules_head(x)
+        values = self.value_head(x)
+        return rules, values
--- ThetaSynthesis/synthon.py
+++ ThetaSynthesis/synthon.py
@@ -1,51 +1,81 @@
-from CGRtools.containers import MoleculeContainer
+from CGRtools import CGRReactor
 from functools import cached_property
-from model import Chem
 from pickle import load
-from torch import FloatTensor
+from torch import FloatTensor, load as torchload
 from typing import Tuple
 from .abc import SynthonABC
+from .source import Chem
+
+
+twohead_model = Chem(2006, 2273)
+twohead_model.load_state_dict(torchload('../source files/twohead_state_dict.pth'))
+twohead_model.eval()
+
+with open('./source files/fitted_fragmentor.pickle', 'rb') as f:
+    fragmentor = load(f)
+with open('./source files/rules_reverse.pickle', 'rb') as f:
+    rules = load(f)
 
 
 class Synthon(SynthonABC):
-    __slots__ = ('_fragmentor', '_model', '__dict__')
+    @property
+    def value(self) -> float:
+        pass
 
-    def __init__(self, molecule):
-        if not isinstance(molecule, MoleculeContainer):
-            raise TypeError('Synthon is only MoleculeContainer')
-        self.__new__(self, molecule)
-        self._model = Model()
-        with open('./source files/fitted_fragmentor.pickle', 'rb') as f:
-            self._fragmentor = load(f)
+    @property
+    def probabilities(self) -> Tuple[float, ...]:
+        pass
+
+    @property
+    def premolecules(self) -> Tuple[Tuple['SynthonABC', ...], ...]:
+        pass
 
     @cached_property
-    def value(self) -> float:
-        return self._neural_network()[1].item()
+    def _get_descriptor(self):
+        return FloatTensor(fragmentor.transform([self._molecule]).values)
+
+
+class CombineSynthon(Synthon):
+    @cached_property
+    def premolecules(self) -> Tuple[Tuple['SynthonABC', ...], ...]:
+        vector = sorted(list(enumerate(self.probabilities)), key=lambda x: x[1], reverse=True)
+        best_100_rules = [rules[x] for x in [y[0] for y in vector[:100]]]
+        out = []
+        for rule in best_100_rules:
+            reactor = CGRReactor(rule, delete_atoms=True)
+            list_products = list(reactor(self._molecule))
+            if list_products:
+                products = []
+                for x in list_products:
+                    products.extend(x.split())
+                out.extend([CombineSynthon(mol) for mol in products])
+        return tuple(out)
 
     @cached_property
     def probabilities(self) -> Tuple[float, ...]:
         return tuple([x.item() for x in self._neural_network()[0][0]])
 
     @cached_property
-    def premolecules(self) -> Tuple[Tuple['SynthonABC', ...], ...]:
-        vector = self.probabilities
-        return
+    def value(self) -> float:
+        return self._neural_network()[1].item()
 
     @cached_property
-    def _get_descriptor(self):
-        return FloatTensor(self._fragmentor.transform([self._molecule]).values)
-
     def _neural_network(self):
-        return self._model(self._get_descriptor)
-
-
-class CombineSynthon(Synthon):
-    ...
+        return twohead_model(self._get_descriptor)
 
 
 class StupidSynthon(Synthon):
-    ...
+    @cached_property
+    def probabilities(self) -> Tuple[float, ...]:
+        return tuple([x.item() for x in self._neural_network()[0][0]])
+
+    @property
+    def value(self):
+        return 1
 
 
 class SlowSynthon(StupidSynthon):
     ...
+
+
+__all__ = ['CombineSynthon', 'SlowSynthon', 'StupidSynthon']
