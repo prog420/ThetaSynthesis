b2aa41a2fe13d7072afb512a4d02e7553f96c276
dcloudf <murkyrussian@gmail.com>
2020-06-09 00:55:11 +0500

    reworked probabilities for synthon

--- ThetaSynthesis/synthon.py
+++ ThetaSynthesis/synthon.py
@@ -2,7 +2,7 @@ from CGRtools import CGRReactor
 from functools import cached_property
 from pickle import load
 from torch import FloatTensor, load as torchload
-from typing import Tuple
+from typing import Tuple, List
 from .abc import SynthonABC
 from .source import Chem
 
@@ -31,7 +31,7 @@ class Synthon(SynthonABC):
         pass
 
     @property
-    def premolecules(self) -> Tuple[Tuple['SynthonABC', ...], ...]:
+    def premolecules(self) -> Tuple[Tuple['Synthon', ...], ...]:
         pass
 
     @cached_property
@@ -41,23 +41,12 @@ class Synthon(SynthonABC):
 
 class CombineSynthon(Synthon):
     @cached_property
-    def premolecules(self) -> Tuple[Tuple['SynthonABC', ...], ...]:
-        vector = sorted(list(enumerate(self.probabilities)), key=lambda x: x[1], reverse=True)
-        best_100_rules = [rules[x] for x in [y[0] for y in vector[:100]]]
-        out = []
-        for rule in best_100_rules:
-            reactor = CGRReactor(rule, delete_atoms=True)
-            list_products = list(reactor(self._molecule))
-            if list_products:
-                products = []
-                for x in list_products:
-                    products.append(x.split())
-                out.append(tuple([CombineSynthon(mol) for mol in products]))
-        return tuple(out)
+    def premolecules(self) -> Tuple[Tuple['Synthon', ...], ...]:
+        return tuple([x[0] for x in self._prods_probs])
 
     @cached_property
     def probabilities(self) -> Tuple[float, ...]:
-        return tuple([x.item() for x in self._neural_network[0][0]])
+        return tuple([x[1] for x in self._prods_probs])
 
     @cached_property
     def value(self) -> float:
@@ -66,6 +55,26 @@ class CombineSynthon(Synthon):
     @cached_property
     def _neural_network(self):
         return twohead_model(self._get_descriptor)
+
+    @property
+    def _probs(self):
+        return tuple([x.item() for x in self._neural_network[0][0]])
+
+    @property
+    def _prods_probs(self):
+        vector = sorted(list(enumerate(self._probs)), key=lambda x: x[1], reverse=True)
+        best_100_rules_with_probs = [(rules[x[0]], x[1]) for x in vector[:100]]
+        out = []
+        for pair in best_100_rules_with_probs:
+            rule, prob = pair
+            reactor = CGRReactor(rule, delete_atoms=True)
+            list_products = list(reactor(self._molecule))
+            if list_products:
+                products = []
+                for x in list_products:
+                    products.append(x.split())
+                out.append([[CombineSynthon(mol[0]) for mol in products], prob])
+        return out
 
 
 class StupidSynthon(Synthon):
