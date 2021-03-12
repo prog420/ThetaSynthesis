7750ca007426c2c48d76e56d82a1dc81e37b1a6e
stsouko <nougmanoff@protonmail.com>
2021-03-12 18:38:07 +0300

    setup and readme. licence.
    tree update.

--- ThetaSynthesis/abc/tree.py
+++ ThetaSynthesis/abc/tree.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2020-2021 Alexander Sizov <>
+#  Copyright 2021 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  This file is part of CGRtools.
+#
+#  ThetaSynthesis is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
 from abc import ABC, abstractmethod
 from CGRtools import ReactionContainer
 from typing import Dict, Set, Tuple
@@ -11,7 +30,7 @@ class ScrollABC(ABC):
 
 
 class RetroTreeABC(ABC):
-    __slots__ = ('_target', '_succ', '_pred', '_nodes', '_depth', '_size', '_free_node')
+    __slots__ = ('_target', '_succ', '_pred', '_nodes', '_depth', '_size', '_free_node', '_visits')
 
     @abstractmethod
     def __init__(self, target: ScrollABC, /, depth: int = 10, size: int = 10000):
@@ -22,8 +41,9 @@ class RetroTreeABC(ABC):
         """
         self._target = target
         self._succ: Dict[int, Set[int]] = {1: set()}
-        self._pred: Dict[int, int] = {1: 1}
+        self._pred: Dict[int, int] = {1: 0}
         self._nodes: Dict[int, ScrollABC] = {1: target}
+        self._visits: Dict[int, int] = {1: 1}
         self._depth = depth
         self._size = size
         self._free_node: int = 1
--- ThetaSynthesis/tree.py
+++ ThetaSynthesis/tree.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2020-2021 Alexander Sizov <>
+#  Copyright 2021 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  This file is part of CGRtools.
+#
+#  ThetaSynthesis is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
 from CGRtools import MoleculeContainer
 from typing import Type
 from .abc import RetroTreeABC, SynthonABC
@@ -5,6 +24,8 @@ from .scroll import Scroll
 
 
 class RetroTree(RetroTreeABC):
+    __slots__ = ()
+
     def __init__(self, target: MoleculeContainer, /, synthon_class: Type[SynthonABC], depth=10, size=10000):
         synthon = synthon_class(target)
         super().__init__(Scroll((synthon,), {synthon}), depth=depth, size=size)
@@ -18,7 +39,18 @@ class RetroTree(RetroTreeABC):
         self._pred[succ] = pred
         self._succ[pred].add(succ)
         self._succ[succ] = set()
+        self._visits[succ] = 1
         self._free_node += 1
+
+    def _update(self, pred: int):
+        """
+        Increment visits count in path from given node to root.
+        """
+        visits = self._visits
+        preds = self._pred
+        while pred:
+            visits[pred] += 1
+            pred = preds[pred]
 
     def __next__(self):
         pass
